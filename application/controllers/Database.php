27da182e548a7292327de219a2c8cd0311fda73c
chevrelo <lorin.chevre@me.com>
2017-12-04 09:22:11 +0100

    Ajout des tables 'users' et 'group' dans la base de données'

--- application/controllers/Database.php
+++ application/controllers/Database.php
@@ -18,5 +18,30 @@ class Database extends CI_Controller {
 			]
 		]);
 		$this->dbforge->create_table('project');
+
+		$this->dbforge->drop_table('users', true);
+		$this->dbforge->add_field("id");
+		$this->dbforge->add_field([
+			'email' => 'VARCHAR',
+			'constraint' => 255
+		],
+			'name' => 'VARCHAR',
+			'constraint' => 255
+		],
+			'password' => 'VARCHAR',
+			'constraint' => 255
+		],
+			'num_group' => 'INTEGER',	// Référence au champ 'id' de la table role
+			'constraint' => 1
+		]);
+		$this->dbforge->create_table('users');
+
+		$this->dbforge->drop_table('group', true);
+		$this->dbforge->add_field('id');
+		$this->dbforge->add_field([
+			'name' => 'VARCHAR',
+			'constraint' => 255
+		])
+		$this->dbforge->create_table('group');
 	}
 }
