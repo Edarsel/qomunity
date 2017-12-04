ec47c0d39b146b7715e79f782b29421437c5c18c
Evan picchi <evan.picchi@rpn.ch>
2017-12-04 15:00:23 +0100

    structure html et visuel css

--- application/controllers/Project.php
+++ application/controllers/Project.php
@@ -7,25 +7,26 @@ class Project extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
-	
+
 	public function add()
 	{
 		$this->load->helper('form');
 		$this->load->library('form_validation');
-		
+
 		$project = (object)[];
 		$project->name = $this->input->post('name');
 		$project->description = $this->input->post('description');
-		
+
 		$this->form_validation->set_rules('name', 'Name', 'trim|required');
 		$this->form_validation->set_rules('description', 'Description', 'trim|required');
 		if($this->form_validation->run())
 		{
 			echo 'OK';
 		}
-		
+
 		$this->load->view('templates/header');
 		$this->load->view('pages/project/add', compact('project'));
 		$this->load->view('templates/footer');
 	}
+
 }
