73f4efd2b3d876c62f95918b740c397ec0659465
Evan picchi <evan.picchi@rpn.ch>
2017-12-04 15:17:26 +0100

    modification du header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,8 +1,24 @@
-<!DOCTYPE html>
+
+<!doctype html>
 <html>
 <head>
-<title>Qommunity</title>
-<meta charset="utf-8">
+<title>Example page</title>
+    <meta charset="utf-8">
+    <link rel = "stylesheet" type = "text/css" href="<?= css_url('style.css') ?>">
+    <link rel = "stylesheet" type = "text/css" href="../../../assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <script src=" script.js"></script>
 </head>
-
 <body>
+    <header>
+        <h1>Qomunity</h1>
+        <nav>
+            <a href="">Page0</a>
+            <a href="">Page1</a>
+            <a href="">Page2</a>
+            <a href="">Page3</a>
+            <a href="">Page4</a>
+            <a href="">Page5</a>
+      </nav>
+    </header>
+    <main>
