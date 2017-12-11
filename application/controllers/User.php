1f8229b60716aed1e8614fbbf9063c1fc443421a
chevrelo <lorin.chevre@rpn.ch>
2017-12-11 11:31:48 +0100

    Modification de la fonction 'add' et création de la fonction 'login' dans le fichier 'User.php'

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -14,81 +14,104 @@ class User extends CI_Controller {
     $this->load->view('welcome_message');
   }
 
-  public function add()
-  {
-    $this->load->helper('form');
-    $this->load->library('form_validation');
-
-    $email = $this->input->post('email');
-    $username = $this->input->post('username');
-    $password = $this->input->post('password');
-
-    $this->form_validation->set_rules('email', 'Adresse email', 'trim|required');
-    $this->form_validation->set_rules('username', 'Utilisateur', 'trim|required');
-    $this->form_validation->set_rules('password', 'Mot de passe', 'trim|required');
-
-    $userInfo = array('email'=>$email, 'username'=>$username,'password'=>password_hash($password, PASSWORD_DEFAULT), 'num_usersGroups'=>1,'num_ranks'=>1);
-
-    if($this->form_validation->run())
+    function add()
     {
-      $id = $this->user_model->add($userInfo);
-      redirect(['user', 'view', $id]);
+        // set form validation rules
+        $this->form_validation->set_rules('username', 'Username','trim|required');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[users.email]');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required|matches[cpassword]');
+        $this->form_validation->set_rules('cpassword', 'Check password', 'trim|required');
+
+        // submit
+        if ($this->form_validation->run() == FALSE)
+        {
+            // fails
+            $this->load->view('pages/user/registration_new');
+        }
+        else
+        {
+
+            $email = $this->input->post('email');
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+
+            $userInfo = array('username'=>$username, 'email'=>$email,'password'=>password_hash($password, PASSWORD_DEFAULT), 'num_usersGroups'=>1,'num_ranks'=>1);
+
+            {
+              $id = $this->user_model->add($userInfo);
+              redirect(['user', 'profile', $id]);
+            }
+
+            if ($this->user_model->insert_user($data))
+            {
+                $this->session->set_flashdata('msg','<div class="alert alert-success text-center">You are Successfully Registered! Please login to access your Profile!</div>');
+                redirect('pages/user/profile');
+            }
+            else
+            {
+                // error
+                $this->session->set_flashdata('msg','<div class="alert alert-danger text-center">Oops! Error.  Please try again later!!!</div>');
+                redirect('pages/user/registration_2');
+            }
+        }
     }
 
-    $this->load->view('templates/header');
-    $this->load->view('pages/user/registration', compact('user'));
-    $this->load->view('templates/footer');
-  }
+    public function login() {
 
-  public function login()
-  {
-    $this->form_validation->set_rules('username', 'Utilisateur', 'trim|required');
-    $this->form_validation->set_rules('password', 'Mot de passe', 'trim|required');
+		// create the data object
+		$data = new stdClass();
 
-    if($this->form_validation->run() == FALSE)
-    {
-      if(isset($this->session->userdata['logged_in']))
-      {
-        $this->load->view('profile');
-      }
+		// load form helper and validation library
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+
+		// set validation rules
+    $this->form_validation->set_rules("email", "Email-ID", "trim|required|xss_clean");
+    $this->form_validation->set_rules("password", "Password", "trim|required|xss_clean");
+
+		if ($this->form_validation->run() == false) {
+
+			// validation not ok, send validation errors to the view
+			$this->load->view('templates/header');
+			$this->load->view('pages/user/login');
+			$this->load->view('templates/footer');
+
+		} else {
+
+			// set variables from the form
+			$email = $this->input->post('email');
+			$password = $this->input->post('password');
+
+			if ($this->user_model->resolve_user_login($email, $password)) {
+
+				$user_id = $this->user_model->get_user_id_from_username($email);
+				$user    = $this->user_model->get_user($user_id);
+
+        //var_dump($user);
+
+				// set session user datas
+				$_SESSION['user']               = $user;
+
+        echo 'Connexion réussie';
+
+				// user login ok
+				$this->load->view('templates/header');
+				$this->load->view('pages/user/profile', $data);
+				$this->load->view('templates/footer');
+
+			}
       else
       {
-        $this->load->view('login');
-      }
-    }
-    else
-    {
-      $data = array(
-        'username' => $this->input->post('username'),
-        'password' => $this->input->post('password')
-      );
-    }
 
-    $result = $this->login_database->login($data);
+				// login failed
+				$data->error = 'Wrong username or password.';
 
-    if ($result == TRUE)
-    {
-      $username = $this->input->post('username');
-      $result = $this->login_database->read_user_information($username);
+				// send error to the view
+				$this->load->view('templates/header');
+				$this->load->view('pages/user/login', $data);
+				$this->load->view('templates/footer');
 
-      if($result != false)
-      {
-        $session_data = array(
-          'username' => $result[0]->username,
-          'email' => $result[0]->email,
-        );
-
-        $this->session->set_userdata('logged_in', $session_data);
-        $this->load->view('profile');
-      }
-    }
-    else
-    {
-      $data = array(
-        'error_message' => 'Invalid Username or Password'
-      );
-
-      $this->load->view('login', $data);
-    }
-  }
+			}
+		}
+	}
 }
