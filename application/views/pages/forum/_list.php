405ba808215cb3fa3483454c861cee8c59593844
Arthur B <arthur.becaud@rpn.ch>
2017-12-11 09:52:34 +0100

    Ajout de l'affichage des groupes dans la vue _list

--- application/views/pages/forum/_list.php
+++ application/views/pages/forum/_list.php
@@ -1,1 +1,8 @@
 <h2>Liste des Groupes</h2>
+<ul class="forum_group_container">
+<?php foreach($groups as $group){?>
+    <li class="forum_group">
+        <h3><?php echo $group->name ;?></h3>
+    </li>
+<?php }?>
+</ul>
