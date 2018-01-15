357111bbae5482f069ff0722943458d7aaefabc0
Yanez Ignacio Alonso <IgnacioAlonso.yanez@rpn.ch>
2018-01-15 08:41:29 +0100

    Résolution de conflits précédemment oubliés

--- application/controllers/Project.php
+++ application/controllers/Project.php
@@ -20,15 +20,9 @@ class Project extends CI_Controller {
 		$this->load->library('form_validation');
 
 		$project = (object)[];
-<<<<<<< HEAD
 		$project->name = trim($this->input->post('name'));
 		$project->description = trim($this->input->post('description'));
-		
-=======
-		$project->name = $this->input->post('name');
-		$project->description = $this->input->post('description');
 
->>>>>>> PicchiEv/master
 		$this->form_validation->set_rules('name', 'Name', 'trim|required');
 		$this->form_validation->set_rules('description', 'Description', 'trim|required');
 		if($this->form_validation->run())
@@ -41,8 +35,7 @@ class Project extends CI_Controller {
 		$this->load->view('pages/project/add', compact('project'));
 		$this->load->view('templates/footer');
 	}
-<<<<<<< HEAD
-	
+
 	public function view($id)
 	{
 		$project = $this->project_model->get($id);
@@ -50,7 +43,4 @@ class Project extends CI_Controller {
 		$this->load->view('pages/project/view', compact('project'));
 		$this->load->view('templates/footer');
 	}
-=======
-
->>>>>>> PicchiEv/master
 }
